7a092ab07ea01f3fd58daef4f973468a693dc699
ravshan2508 <ravshan@gmx.com>
2022-09-03 05:18:55 +0900

    reja_project

--- server.js
+++ server.js
@@ -2,6 +2,16 @@ console.log('Web Serverni boshlash');
 const express = require('express');
 const app = express();
 const http = require('http');
+const fs = require('fs');
+
+let user;
+fs.readFile("database/user.json","utf8",(err,data) => {
+    if (err) {
+        console.log('ERROR',err);
+    } else {
+        user = JSON.parse(data)
+    }
+});
 //1 Kirish kodlari
 app.use(express.static("public"));
 app.use(express.json());
@@ -13,15 +23,24 @@ app.set("views","views");
 app.set("view engine","ejs");
 
 //4 Routing code
+/* app.post("/create-item", (req,res)=>{
+    //
+}); */
 
-app.get("/",function(req, res) {
-    res.end(`<h1 style="background: violet">Hello World by Ravshan</h1>`);
+app.get("/author",(req,res) => {
+    res.render("author", {user : user});
+
 });
+
+app.get("/",function(req , res) {
+    res.render("harid");
+});
+
 
 const server = http.createServer(app);
 let PORT = 3000;
 server.listen(PORT, function () {
-    console.log(`This server is runnig sucsessfully on port: ${PORT}`);
+    console.log(`http://localhost:3000 This server is runnig sucsessfully on port: ${PORT}`);
 });
 
 
